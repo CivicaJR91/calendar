f22c46429954088c50b698f986e6ba9e4683923f
Janliz Suarez <jsuarezrosado@gmail.com>
2020-11-01 20:41:50 -0600

    localstore

--- develop/javascript.js
+++ develop/javascript.js
@@ -5,35 +5,33 @@ date.innerHTML = moment().format('MMMM Do, YYYY');
 
 
 var hours = moment().format('h:mm a');
-var time = document.getElementById("daytime");
-var dailyTask = document.getElementById("task");
-var saveTaskButton = document.getElementById("savebutton");
+var time = document.querySelector(".daytime");
+var dailyTask = document.querySelector(".task");
+var saveTaskButton = document.querySelector(".savebutton");
 var rowBlock = document.querySelector(".time-block")
-
-var workTime= ["9:00 AM","10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"];
 
 
 //LocalStorage
 // Store Tasks
-function storeTask () {
-  localStorage.setItem("task", JSON.stringify(dailyTask));
 
-}
 
 // adding eventlistener to the block that holds the time, text, and save button
-rowBlock.addEventListener("click", function(event) {
-event.preventDefault();
+saveTaskButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-dailyTask.value.trim();
+  
+console.log(event.target.getAttribute("data"));
 
-storeTask();
-renderTasks();
+localStorage.setItem("task", JSON.stringify(event.target.getAttribute("data")));
+
+  renderTasks();
+
 });
 
 // 
-function renderTasks (){
+function renderTasks() {
 
-JSON.parse (localStorage.getItem ("task"));
+  JSON.parse(localStorage.getItem("task"));
 }
 
 
